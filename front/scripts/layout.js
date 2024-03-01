20c23bbff8e628540f31295b2b8372c3dca0d485
Frank GP <fgp555@gmail.com>
2024-03-01 10:14:10 -0500

    L1 - layout dinamic

--- front/scripts/layout.js
+++ front/scripts/layout.js
@@ -1,3 +1,14 @@
+var newDate_getTime = new Date().getTime();
+
+const head = `
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link rel="stylesheet" href="styles/reset.css?${newDate_getTime}" />
+<link rel="stylesheet" href="styles/main.css?${newDate_getTime}" />
+`;
+
+document.head.innerHTML += head;
+
 const header = `
 <h2><a href="./">Cinema</a></h2>
 <nav>
--- front/scripts/main.js
+++ front/scripts/main.js
@@ -1,20 +1,19 @@
 const movieListContainer = document.getElementById("movieList");
 
 const movieElements = tempData.map((movie) => {
-  const movieDiv = document.createElement("div");
-  movieDiv.classList.add("card_item");
-
-  movieDiv.innerHTML = `
-    <img src="${movie.poster}" alt="${movie.title}">
-    <h3>${movie.title}</h3>
+  return /* html */ `
+  <div class="card_item">
+    <a href="#">
+      <img src="${movie.poster}" alt="${movie.title}">
+      <h3>${movie.title}</h3>
+    </a>
     <p>Year: ${movie.year}</p>
     <p>Director: ${movie.director}</p>
     <p>Duration: ${movie.duration}</p>
     <p>Genre: ${movie.genre.join(", ")}</p>
     <p>Rating: ${movie.rate}</p>
+  </div>
   `;
-
-  movieListContainer.appendChild(movieDiv);
-
-  return movieDiv;
 });
+
+movieListContainer.innerHTML = movieElements.join("");
