40a33eece0c8194c23ee77f970c69d4e1d947b4d
Frank GP <fgp555@gmail.com>
2024-03-07 19:28:35 -0500

    lecture 3 bootstrap

--- front/scripts/main.js
+++ front/scripts/main.js
@@ -1,14 +1,19 @@
 // var url = "https://students-api.2.us-1.fl0.io/movies";
-var url = "https://webpt19b.web.app/data/movies.json";
+// var url = "https://webpt19b.web.app/data/movies.json";
+var url = "https://webpt19b.cyclic.app/movies";
 
 const movieListContainer = document.getElementById("movieList");
 
 $.get(url, (data, status) => {
   console.log(status);
+  renderCard(data);
+});
+
+const renderCard = (data) => {
   const movieElements = data.map((movie) => {
     return /* html */ `
             <div class="card_item">
-              <a href="#">
+              <a href="${movie.poster}">
                 <img src="${movie.poster}" alt="${movie.title}">
                 <h3>${movie.title}</h3>
               </a>
@@ -22,4 +27,4 @@ $.get(url, (data, status) => {
   });
 
   movieListContainer.innerHTML = movieElements.join("");
-});
+};
