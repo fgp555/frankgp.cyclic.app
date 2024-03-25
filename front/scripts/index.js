6a338501447a476ec8c0a55a5cebbb243be9453f
Frank GP <fgp555@gmail.com>
2024-03-24 22:43:47 -0500

    L-06 Asincronismo

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,8 +1,18 @@
 const renderMovieCards = require("./renderCards.js");
+const axios = require("axios");
 
 // var url = "https://students-api.2.us-1.fl0.io/movies";
 var url = "https://api.1rodemayo.com/movies";
-$.get(url, (data, status) => {
-  renderMovieCards(data);
-  console.log(status);
-});
+
+// $.get(url, (data, status) => {
+//   renderMovieCards(data);
+//   console.log(status);
+// });
+
+const fetchData = async () => {
+  const response = await axios.get(url);
+  console.log(response.data);
+  renderMovieCards(response.data);
+};
+
+fetchData();
